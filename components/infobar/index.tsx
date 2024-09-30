b8b76f74bb06ef1e24a8022b67e29e52d0328d6a
Mario Teklic <m.teklic@akkusys.de>
2024-09-30 21:42:04 +0200

    initial

--- components/infobar/index.tsx
+++ components/infobar/index.tsx
@@ -2,10 +2,13 @@
 import React, { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Search } from 'lucide-react'
+import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from '@/components/ui/command'
+import { Button } from '../ui/button';
 
 const InfoBar = () => {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
+  const [open, setOpen] = useState(false);
 
   const handleSearchClick = () => {
     setIsSearchOpen(true);
@@ -24,15 +27,36 @@ const InfoBar = () => {
   return (
     <header className="flex items-center justify-between p-4 border-b">
       <div className="flex-1 flex justify-center">
-      <div className="relative">
-            <input
-              type="text"
-              placeholder="Quick Search"
-              className="pl-10 pr-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              onClick={handleSearchClick}
-            />
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
-          </div>
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Quick Search"
+            className="pl-10 pr-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            onClick={() => setOpen(!open)}
+          />
+          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
+        </div>
+      </div>
+      <div>
+
+        <CommandDialog open={open} onOpenChange={setOpen}>
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Suggestions">
+              <CommandItem>Calendar</CommandItem>
+              <CommandItem>Search Emoji</CommandItem>
+              <CommandItem>Calculator</CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Settings">
+              <CommandItem>Invoices</CommandItem>
+              <CommandItem>Profile</CommandItem>
+              <CommandItem>Billing</CommandItem>
+              <CommandItem>Settings</CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </CommandDialog>
       </div>
 
       {isSearchOpen && (
